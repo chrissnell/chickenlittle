378f6f1f0856b804ae95881662bc9e09e81b98d5
Christopher Snell <chris.snell@gmail.com>
2015-06-09 11:01:31 -0700

    Fix an issue where the last step of the plan wouldn't repeat

--- notification.go
+++ notification.go
@@ -20,6 +20,8 @@ type NotificationsInProgress struct {
 	Mu            sync.Mutex
 }
 
+// Start the main notification loop.  This loop receives notifications on planChan and launches the notificationHandler
+// to carry out the actual notifications.  Also receives requests to stop notifications on stopChan and then stops them.
 func StartNotificationEngine() {
 	// Initialize our map of Stopper channels
 	// UUID -> channel
@@ -76,6 +78,8 @@ func StartNotificationEngine() {
 
 }
 
+// Receives notification requests from the notification engine and steps through the plan, making phone calls,
+// sending SMS, email, etc., as necessary.
 func notificationHandler(nr *NotificationRequest, sc <-chan bool) {
 
 	var timerChan <-chan time.Time
@@ -84,8 +88,10 @@ func notificationHandler(nr *NotificationRequest, sc <-chan bool) {
 	uuid := nr.Plan.ID.String()
 	log.Println("[", uuid, "]", "Initiating notification plan")
 
+	// Iterate through each step of the plan
 	for n, s := range nr.Plan.Steps {
 
+		// TO DO: validate Method here and return an error if it's unsupported
 		u, err := url.Parse(s.Method)
 		if err != nil {
 			log.Println("Error parsing URI:", err)
@@ -95,45 +101,57 @@ func notificationHandler(nr *NotificationRequest, sc <-chan bool) {
 
 		log.Println("[", uuid, "]", "Method:", s.Method)
 
-		switch u.Scheme {
-		case "phone":
-			MakePhoneCall(u.Host, nr.Content, uuid)
-		case "sms":
-			SendSMS(u.Host, nr.Content, uuid, false)
-		case "email":
-			SendEmail(fmt.Sprint(u.User, "@", u.Host), nr.Content, uuid)
-		}
-
-		if n == len(nr.Plan.Steps)-1 {
-			// Last step, so we use a Ticker and NotifyEveryPeriod
-			tickerChan = time.NewTicker(s.NotifyEveryPeriod).C
-			log.Println("[", uuid, "]", "Scheduling the next retry in", strconv.FormatFloat(s.NotifyEveryPeriod.Minutes(), 'f', 1, 64), "minutes")
-
-		} else {
-			// Not the last step, so we use a Timer and NotifyUntilPeriod
-			timerChan = time.NewTimer(s.NotifyUntilPeriod).C
-			log.Println("[", uuid, "]", "Scheduling the next notification step in", strconv.FormatFloat(s.NotifyUntilPeriod.Minutes(), 'f', 1, 64), "minutes")
-		}
-
-	timerLoop:
+	stepLoop:
+		// This outer loop repeats a notification until it's acknowledged.  It can be broken by the expiration of the timer for this step,
+		// or by a stop request.
 		for {
-			select {
-			case <-timerChan:
-				log.Println("[", uuid, "]", "Step timer expired.  Proceeding!")
-				break timerLoop
-			case <-tickerChan:
-				log.Println("[", uuid, "]", "**Tick**  Retry contact method!")
-				log.Println("[", uuid, "]", "Waiting", strconv.FormatFloat(s.NotifyEveryPeriod.Minutes(), 'f', 1, 64), "minutes]")
-			case <-sc:
-				log.Println("[", uuid, "]", "Stop request received.  Terminating notifications.")
-				NIP.Mu.Lock()
-				defer NIP.Mu.Unlock()
-				delete(NIP.Stoppers, uuid)
-				delete(NIP.Messages, uuid)
-				return
-			}
-		}
 
-		log.Println("Loop broken")
+			// Take the appropriate action, depending on the type of notification
+			switch u.Scheme {
+			case "phone":
+				MakePhoneCall(u.Host, nr.Content, uuid)
+			case "sms":
+				SendSMS(u.Host, nr.Content, uuid, false)
+			case "email":
+				SendEmail(fmt.Sprint(u.User, "@", u.Host), nr.Content, uuid)
+			}
+
+			if n == len(nr.Plan.Steps)-1 {
+				// We're at the last step of the plan, so this step will repeat until ackknowledged. We use a Ticker and set its period to NotifyEveryPeriod
+				tickerChan = time.NewTicker(s.NotifyEveryPeriod).C
+				log.Println("[", uuid, "]", "Scheduling the next retry in", strconv.FormatFloat(s.NotifyEveryPeriod.Minutes(), 'f', 1, 64), "minutes")
+
+			} else {
+				// We're not at the last step, so we only run this step once.  We use Timer set its duration to NotifyUntilPeriod
+				timerChan = time.NewTimer(s.NotifyUntilPeriod).C
+				log.Println("[", uuid, "]", "Scheduling the next notification step in", strconv.FormatFloat(s.NotifyUntilPeriod.Minutes(), 'f', 1, 64), "minutes")
+			}
+
+		timerLoop:
+			// This inner loop selects over various channels to receive timers and stop requests.
+			// It can be broken by an expiring Timer (signaling that it's time to proceed to the next step) or a stop request.
+			for {
+				select {
+				case <-timerChan:
+					// Our timer for this step has expired so we break the outer loop to proceed to the next step.
+					log.Println("[", uuid, "]", "Step timer expired.  Proceeding to next plan step.")
+					break stepLoop
+				case <-tickerChan:
+					// Our ticker for this step expired, so we'll break the inner loop and try this step again.
+					log.Println("[", uuid, "]", "**Tick**  Retry contact method!")
+					log.Println("[", uuid, "]", "Waiting", strconv.FormatFloat(s.NotifyEveryPeriod.Minutes(), 'f', 1, 64), "minutes]")
+					break timerLoop
+				case <-sc:
+					log.Println("[", uuid, "]", "Stop request received.  Terminating notifications.")
+					NIP.Mu.Lock()
+					defer NIP.Mu.Unlock()
+					delete(NIP.Stoppers, uuid)
+					delete(NIP.Messages, uuid)
+					return
+				}
+			}
+
+			log.Println("Loop broken")
+		}
 	}
 }
